305ccb0534bd33d3722da60e57426f301f0e454e
cst522 <r202302005sb@jindai.jp>
2023-11-05 22:09:09 +0900

    20231105

--- de12/python/test2.py
+++ de12/python/test2.py
@@ -1,7 +1,9 @@
+# Code InportError になってrequestが使えなかった
+#urllib.request は URLs (Uniform Resource Locators) を取得するための Python モジュールである
 import urllib.request
 import json
 
-# OpenWeatherMap APIキーを取得してください
+# OpenWeatherMap APIキーを取得して入力
 api_key = '2ac00926ab84ac46e5872e550aa4a2de'
 city = 'Yokohama,JP'
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
@@ -15,3 +17,6 @@ try:
     print(f'横浜の気圧: {pressure} hPa')
 except urllib.error.URLError as e:
     print('気圧情報の取得に失敗しました。エラー:', e)
+    if pressure is not None:
+        if pressure <= 1007:
+            print('気圧が1007hPa以下になりました。頭痛注意！')
