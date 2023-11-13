5cee57bef7e841e7bad84d1f5fc02ab54f9d7f96
cst522 <r202302005sb@jindai.jp>
2023-11-13 11:03:39 +0900

    20231113

--- de12/python/xbp_suumo-main/main.py
+++ de12/python/xbp_suumo-main/main.py
@@ -16,10 +16,10 @@ df = suumo.read_csv("./data/yokohama_kawasaki.csv", index_col=0, encoding="utf-8
 
 # データ分析
 # df.n_rooms("")
-df.n_rooms_by_line(["JR線","横浜線"], "築年数")
+#df.n_rooms_by_line(["ＪＲ線","京葉線"], "家賃")
 
 # df.average_bar("間取り", "面積")
 # df.average_bar_by_line(["目黒駅", "京急逗子線"], "間取り", "面積")
 
 # df.scatter_line("面積", "合計時間")
-# df.scatter_station(["京急本線", "京急逗子線"], "面積", "合計時間")
+df.scatter_station(["ＪＲ京葉線"], "構造", "家賃")
